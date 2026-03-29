105c3f387cfd454682eee7f0af904c0e06cc18a6
qiuqiueyu <1694697482@qq.com>
2026-03-29 17:18:11 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,17 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Guide from '../components/Guide.vue'
+import Home from '../components/Home.vue'
+import Word from '../components/Word.vue'
 
+const routes = [
+  { path: '/', component: Guide },
+  { path: '/home', component: Home },
+  { path: '/word', component: Word }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
