4ebd323955400d17ac7b0c42f17cf8384567cb7b
qiuqiueyu <1694697482@qq.com>
2026-03-29 17:14:39 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 只保留路由出口，用来渲染不同页面 -->
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+/* 全局样式重置，避免页面滚动、边距问题 */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html, body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
